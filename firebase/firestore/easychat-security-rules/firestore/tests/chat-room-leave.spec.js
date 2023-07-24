75cdd44d0069635db1aaa93d68284ff617027e49
JaeHo Song <thruthesky@gmail.com>
2023-07-24 18:38:01 +0800

    updating on easy chat rule

--- firebase/firestore/easychat-security-rules/firestore/tests/chat-room-leave.spec.js
+++ firebase/firestore/easychat-security-rules/firestore/tests/chat-room-leave.spec.js
@@ -4,114 +4,218 @@ const { db, a, b, c, d, tempChatRoomData, admin } = require("./setup");
 // load firebase-functions-test SDK
 const firebase = require("@firebase/testing");
 
-describe("Chat room leave", () => {
-    it("Chat room leave - failure test: b removes a", async () => {
+describe("Chat room leave, and kickout", () => {
+  it("Chat room leave - failure test: b removes a", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid] }));
+    snapshot = await ref.get();
 
-        // create a chat room
-        const ref = await admin()
-            .collection("easychat")
-            .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid] }));
-        
-        snapshot = await ref.get();
+    // chat room leave
+    await firebase.assertFails(
+      db(b)
+        .collection("easychat")
+        .doc(ref.id)
+        .update({
+          users: firebase.firestore.FieldValue.arrayRemove(a.uid),
+        })
+    );
+  });
 
-        // console.log(snapshot.data());
+  it("Chat room leave - failure test: b removes a from users araay that has manu muliple users. -fails", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(
+        tempChatRoomData({
+          master: a.uid,
+          users: [
+            a.uid,
+            b.uid,
+            c.uid,
+            d.uid,
+            a.uid,
+            b.uid,
+            b.uid,
+            b.uid,
+            b.uid,
+            c.uid,
+            c.uid,
+            c.uid,
+            c.uid,
+          ],
+        })
+      );
+    snapshot = await ref.get();
 
+    // chat room leave
+    await firebase.assertFails(
+      db(b)
+        .collection("easychat")
+        .doc(ref.id)
+        .update({
+          users: firebase.firestore.FieldValue.arrayRemove(a.uid),
+        })
+    );
+  });
 
-        // chat room leave
-        await firebase.assertFails(
-            db(b).collection("easychat").doc(ref.id).update({
-                users: firebase.firestore.FieldValue.arrayRemove(a.uid)
-            })
-        )
-    });
-    it("Chat room leave - failure test: b removes a from users araay that has manu muliple users.", async () => {
+  it("Chat room leave - failure test: b removes b from users araay that has manu muliple users. succeeds", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(
+        tempChatRoomData({
+          master: a.uid,
+          users: [a.uid, b.uid, c.uid, d.uid, b.uid, b.uid],
+        })
+      );
 
-        // create a chat room
-        const ref = await admin()
-            .collection("easychat")
-            .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid, c.uid, d.uid, a.uid, b.uid, b.uid, b.uid, b.uid, c.uid, c.uid, c.uid, c.uid] }));
-        
-        snapshot = await ref.get();
+    // chat room leave
+    await firebase.assertSucceeds(
+      db(b)
+        .collection("easychat")
+        .doc(ref.id)
+        .update({
+          users: firebase.firestore.FieldValue.arrayRemove(b.uid),
+        })
+    );
+  });
 
-        // console.log(snapshot.data());
+  it("Master can remove a user from user list - succeed", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(
+        tempChatRoomData({
+          master: a.uid,
+          users: [a.uid, b.uid, c.uid, d.uid, b.uid, b.uid],
+        })
+      );
 
+    // chat room leave
+    await firebase.assertSucceeds(
+      db(a)
+        .collection("easychat")
+        .doc(ref.id)
+        .update({
+          users: firebase.firestore.FieldValue.arrayRemove(b.uid),
+        })
+    );
+  });
 
-        // chat room leave
-        await firebase.assertFails(
-            db(b).collection("easychat").doc(ref.id).update({
-                users: firebase.firestore.FieldValue.arrayRemove(a.uid)
-            })
-        )
-    });
-    it("Chat room leave - failure test: b removes b from users araay that has manu muliple users.", async () => {
+  it("Moderator can remove a user from user list - succeed", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(
+        tempChatRoomData({
+          master: a.uid,
+          moderators: [d.uid],
+          users: [a.uid, b.uid, c.uid, d.uid, b.uid, b.uid],
+        })
+      );
 
-        // create a chat room
-        const ref = await admin()
-            .collection("easychat")
-            .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid, c.uid, d.uid, b.uid, b.uid ] }));
-        
-        // oldSanpshot = await ref.get();
+    // chat room leave
+    await firebase.assertSucceeds(
+      db(d)
+        .collection("easychat")
+        .doc(ref.id)
+        .update({
+          users: firebase.firestore.FieldValue.arrayRemove(b.uid),
+        })
+    );
+  });
 
-        // console.log("Before ---> ", oldSanpshot.data());
+  it("Moderator should not be able to remove the master from user list - fail", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(
+        tempChatRoomData({
+          master: a.uid,
+          moderators: [d.uid],
+          users: [a.uid, b.uid, c.uid, d.uid, b.uid, b.uid],
+        })
+      );
 
+    // chat room leave
+    await firebase.assertFails(
+      db(d)
+        .collection("easychat")
+        .doc(ref.id)
+        .update({
+          users: firebase.firestore.FieldValue.arrayRemove(a.uid),
+        })
+    );
+  });
 
-        // chat room leave
-        await firebase.assertSucceeds(
-            db(b).collection("easychat").doc(ref.id).update({
-                users: firebase.firestore.FieldValue.arrayRemove(b.uid)
-            })
-        )
+  it("Moderator should not be able to remove the master from user list - fail", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(
+        tempChatRoomData({
+          master: a.uid,
+          moderators: [d.uid],
+          users: [a.uid, b.uid, c.uid, d.uid],
+        })
+      );
 
-        // afterSnapshot = await ref.get();
-        // console.log("After ---> ", afterSnapshot.data());
-    });
+    // chat room leave
+    await firebase.assertFails(
+      db(d)
+        .collection("easychat")
+        .doc(ref.id)
+        .update({
+          users: firebase.firestore.FieldValue.arrayRemove(a.uid),
+        })
+    );
+  });
 
+  it("User should not be able to remove the moderator from user list - fail", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(
+        tempChatRoomData({
+          master: a.uid,
+          moderators: [d.uid],
+          users: [a.uid, b.uid, c.uid, d.uid],
+        })
+      );
 
-    it("Master can remove a user from user list", async () => {
+    // chat room leave
+    await firebase.assertFails(
+      db(c)
+        .collection("easychat")
+        .doc(ref.id)
+        .update({
+          users: firebase.firestore.FieldValue.arrayRemove(d.uid),
+        })
+    );
+  });
 
-        // create a chat room
-        const ref = await admin()
-            .collection("easychat")
-            .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid, c.uid, d.uid, b.uid, b.uid ] }));
-        
-        // chat room leave
-        await firebase.assertSucceeds(
-            db(a).collection("easychat").doc(ref.id).update({
-                users: firebase.firestore.FieldValue.arrayRemove(b.uid)
-            })
-        )
-    });
-    it("Moderator can remove a user from user list", async () => {
+  it("User should not be able to remove the master from user list - fail", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(
+        tempChatRoomData({
+          master: a.uid,
+          moderators: [d.uid],
+          users: [a.uid, b.uid, c.uid, d.uid],
+        })
+      );
 
-        // create a chat room
-        const ref = await admin()
-            .collection("easychat")
-            .add(tempChatRoomData({ master: a.uid, moderators: [d.uid], users: [a.uid, b.uid, c.uid, d.uid, b.uid, b.uid ] }));
-        
-        // chat room leave
-        await firebase.assertSucceeds(
-            db(d).collection("easychat").doc(ref.id).update({
-                users: firebase.firestore.FieldValue.arrayRemove(b.uid)
-            })
-        )
-    });
-    
-
-
-    it("Moderator can remove a user from user list", async () => {
-
-        // create a chat room
-        const ref = await admin()
-            .collection("easychat")
-            .add(tempChatRoomData({ master: a.uid, moderators: [d.uid], users: [a.uid, b.uid, c.uid, d.uid, b.uid, b.uid ] }));
-        
-        // chat room leave
-        await firebase.assertFails(
-            db(d).collection("easychat").doc(ref.id).update({
-                users: firebase.firestore.FieldValue.arrayRemove(a.uid)
-            })
-        )
-    });
-
+    // chat room leave
+    await firebase.assertFails(
+      db(c)
+        .collection("easychat")
+        .doc(ref.id)
+        .update({
+          users: firebase.firestore.FieldValue.arrayRemove(a.uid),
+        })
+    );
+  });
 });
-
