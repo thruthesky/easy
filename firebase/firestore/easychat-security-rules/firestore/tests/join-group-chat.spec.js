455c9137ad3a1be540232666f7e43e3b9d721019
Christian Bucal <withcenter-dev2@github.com>
2023-07-25 13:38:32 +0800

    join and invite test

--- firebase/firestore/easychat-security-rules/firestore/tests/join-group-chat.spec.js
+++ firebase/firestore/easychat-security-rules/firestore/tests/join-group-chat.spec.js
@@ -1,35 +1,70 @@
 const assert = require("assert");
-const { db, a, b, tempChatRoomData } = require("./setup");
+const { db, a, b, c, admin, tempChatRoomData } = require("./setup");
 
 // load firebase-functions-test SDK
 const firebase = require("@firebase/testing");
 
 describe("Join test", () => {
-  // it("Join the room with open: false -> error test", async () => {
-  //   // Create a chat room by a.
-  //   // The chat rooom is closed by default.
-  //   const ref = await firebase.assertSucceeds(
-  //     db(a)
-  //       .collection("easychat")
-  //       .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid] }))
-  //   );
-  // });
-  // it("Join the room with open: true -> success test", async () => {
-  //   // Create a chat room by a.
-  //   // The chat rooom is closed by default.
-  //   const ref = await firebase.assertSucceeds(
-  //     db(a)
-  //       .collection("easychat")
-  //       .add(tempChatRoomData({ master: a.uid, open: true }))
-  //   );
-  //   // Join the room as b.
-  //   await firebase.assertSucceeds(
-  //     db(b)
+
+  it("Join to the 1:1 room, group false, open false -> error test", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid], group: false, open: false }));
+
+    // Join the room as C.
+    await firebase.assertFails(
+      db(c)
+        .collection("easychat")
+        .doc(ref.id)
+        .set({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
+    );
+  });
+
+  it("Join the room, group true, open true -> success test", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid], group: true, open: true }));
+
+    // Join the room as C.
+    await firebase.assertSucceeds(
+      db(c)
+        .collection("easychat")
+        .doc(ref.id)
+        .set({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
+    );
+  });
+
+  it("Joining to the closed room, group true, open false -> fail test", async () => {
+    // create a chat room
+    const ref = await admin()
+      .collection("easychat")
+      .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid], group: true, open: false }));
+    // Join the room as C.
+    await firebase.assertFails(
+      db(c)
+        .collection("easychat")
+        .doc(ref.id)
+        .set({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
+    );
+  });
+
+  // ! This is for confirmation
+  // Because, hackers may learn that they can actually join rooms that are not group but open (true)
+  // it("Joining to the closed room, group false, open true -> fail test", async () => {
+  //   // create a chat room
+  //   const ref = await admin()
+  //     .collection("easychat")
+  //     .add(tempChatRoomData({ master: a.uid, users: [a.uid, b.uid], group: false, open: true }));
+  //   // Join the room as C.
+  //   await firebase.assertFails(
+  //     db(c)
   //       .collection("easychat")
   //       .doc(ref.id)
-  //       .collection("users")
-  //       .doc(b.uid)
-  //       .set({ uid: b.uid, createdAt: new Date() })
+  //       .set({ users: firebase.firestore.FieldValue.arrayUnion(c.uid) })
   //   );
   // });
+
+
 });
