1bc90eba59a887598e6555c43cb21232bf6eeb12
Christian Bucal <withcenter-dev2@github.com>
2023-07-26 17:58:24 +0800

    firestore rules problem, for help with mr song, [block user in chat]

--- firebase/firestore/easychat-security-rules/firestore/tests/message-read.spec.js
+++ firebase/firestore/easychat-security-rules/firestore/tests/message-read.spec.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const { db, a, b, c, tempChatRoomData, createChatRoom, block } = require("./setup");
+const { db, a, b, c, tempChatRoomData, createChatRoom, block, unblock, invite } = require("./setup");
 
 // load firebase-functions-test SDK
 const firebase = require("@firebase/testing");
@@ -51,6 +51,54 @@ describe("Message read test", () => {
 
         await firebase.assertFails(
             db(b)
+                .collection("easychat")
+                .doc(roomRef.id)
+                .collection("messages")
+                .get()
+        );
+    });
+
+    it("Read Group chat if the user is unblocked - success", async () => {
+
+        // user A created a Room
+        const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid], open: false, group: true });
+
+        // user A invited user B
+        await invite(a, b, roomRef.id);
+
+        // user A blocked B
+        await block(a, b, roomRef.id);
+
+        // user A unblocked B
+        await unblock(a, b, roomRef.id);
+
+        // user B should see the messages
+        await firebase.assertSucceeds(
+            db(b)
+                .collection("easychat")
+                .doc(roomRef.id)
+                .collection("messages")
+                .get()
+        );
+    });
+
+    it("Read Group chat if another user is blocked - success", async () => {
+
+        // user A created a Room
+        const roomRef = await createChatRoom(a, { master: a.uid, users: [a.uid], open: false, group: true });
+
+        // user A invited user B
+        await invite(a, b, roomRef.id);
+
+        // user A invited user C
+        await invite(a, c, roomRef.id);
+
+        // user A blocked B
+        await block(a, b, roomRef.id);
+
+        // user C should see the messages
+        await firebase.assertSucceeds(
+            db(c)
                 .collection("easychat")
                 .doc(roomRef.id)
                 .collection("messages")
